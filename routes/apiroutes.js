4d9f5816a81d333116dccf740242f5198035d2ad
kjdgac <kelly.davis@gulfstream.com>
2021-03-25 17:56:25 -0400

    data collects cell content, not dom object; page.evaluate inside page.goto try block; browser.close called on errors

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -18,45 +18,48 @@ module.exports = function(app) {
         try {
             await page.goto(`${stationPage}${req.params.id}&reportdate=${year}-${month < 10 ? `0${month + 1}` : month + 1}-${date < 10 ? `0${date}` : date}`)
         // navigate to requested page
+        
+            try {
+                const data = { 
+                        actualDir1:[],
+                        histDir1:[],
+                        speedDir1:[],
+                        actualDir2:[],
+                        histDir2:[],
+                        speedDir2:[]
+                };
+                await page.evaluate( ()=>{
+                    const table = document.getElementById("tableContainer").getElementsByTagName("table")[0];
+                    const rows = table.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
+                    console.log("table:",table);
+                    console.log("rows",rows);
+                    data.dirNames = [table.getElementsByTagName("th")[1].textContent, table.getElementsByTagName("th")[2].textContent]
+                    for (let i=0; i<rows.length; i++){
+                        let cells = rows[i].getElementsByTagName("td");
+                        data.actualDir1.push(cells[1].textContent);
+                        data.histDir1.push(cells[2].textContent);
+                        data.speedDir1.push(cells[3].textContent);
+                        data.actualDir2.push(cells[5].textContent);
+                        data.histDir2.push(cells[6].textContent);
+                        data.speedDir2.push(cells[7].textContent);
+                    }   
+                });
+                console.log(data)
+                await browser.close();
+                // clean up
+
+                res.status(200).set("Access-Control-Allow-Origin","*").json( data );
+            }
+            catch(err) {
+                console.log(err); 
+                await browser.close();
+                res.set("Access-Control-Allow-Origin","*").send(err);
+            };
         }
         catch(err){
             console.log(err); 
-            res.set("Access-Control-Allow-Origin","*").send(err);
-        }
-
-        try {
-            let data;
-            await page.evaluate( ()=>{
-                let table = document.getElementById("tableContainer").getElementsByTagName("table")[0];
-                let rows = table.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
-                data = { 
-                    actualDir1:[],
-                    histDir1:[],
-                    speedDir1:[],
-                    actualDir2:[],
-                    histDir2:[],
-                    speedDir2:[],
-                    dirNames: [table.getElementsByTagName("th")[1].textContent, table.getElementsByTagName("th")[2].textContent]
-                }
-                for (let i=0; i<rows.length; i++){
-                    data.actualDir1.push(rows[i].getElementsByTagName("td")[1]);
-                    data.histDir1.push(rows[i].getElementsByTagName("td")[2]);
-                    data.speedDir1.push(rows[i].getElementsByTagName("td")[3]);
-                    data.actualDir2.push(rows[i].getElementsByTagName("td")[5]);
-                    data.histDir2.push(rows[i].getElementsByTagName("td")[6]);
-                    data.speedDir2.push(rows[i].getElementsByTagName("td")[7]);
-                }
-                
-            });
-            console.log(data)
             await browser.close();
-            // clean up
-
-            res.status(200).set("Access-Control-Allow-Origin","*").json( data );
-        }
-        catch(err) {
-            console.log(err); 
             res.set("Access-Control-Allow-Origin","*").send(err);
-        };
+        }
     });
 };
